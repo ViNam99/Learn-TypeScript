37741c240e434c48116b45ec7e58d5132d18dec5
Nhan Vi Nam <nhanvinamcoder@gmail.com>
2020-09-19 13:42:49 +0700

    Resparameter

--- jsFile/index.js
+++ jsFile/index.js
@@ -1,2 +1,37 @@
 "use strict";
-alert("hello Word");
+// Các kiểu dữ liệu
+var trangThai = true;
+var tong = 1;
+var title = "Nam";
+var mangNguoiDung = [112, 3, 5];
+var mangKhoaHoc = ["1", 2, true];
+var mangThanhVien = ["1", 1]; //tuple
+// Arrrow Fucntion
+//Nếu ko khai báo 2,3 ở dưới thì sẽ lấy 10 + 20
+// const tinhTong = (a: number = 10, b: number = 20) => a + b;
+// Ưu tiên hơn: Giá trị mặc định cảu tham số function
+// console.log(tinhTong(2, 3));
+//Resparameter
+// const dispayColors = (...colors: Array<string>) => {
+//   return colors.map((color) => {
+//     console.log(color);
+//   });
+// };
+// const dispayColors = (...colors: string[]) => {
+//   return colors.map((color) => {
+//     console.log(color);
+//   });
+// };
+// dispayColors("red", "green");
+var tinhTongParams = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    var tong = 0;
+    numbers.forEach(function (num) {
+        tong += num;
+    });
+    return tong;
+};
+console.log(tinhTongParams(1, 2, 3, 4, 5));
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,39 @@
-alert("hello Word")
+// Các kiểu dữ liệu
+let trangThai: boolean = true;
+let tong: number = 1;
+let title: string = "Nam";
+let mangNguoiDung: number[] = [112, 3, 5];
+let mangKhoaHoc: Array<any> = ["1", 2, true];
+let mangThanhVien: [string, number] = ["1", 1]; //tuple
+
+// Arrrow Fucntion
+//Nếu ko khai báo 2,3 ở dưới thì sẽ lấy 10 + 20
+// const tinhTong = (a: number = 10, b: number = 20) => a + b;
+
+// Ưu tiên hơn: Giá trị mặc định cảu tham số function
+// console.log(tinhTong(2, 3));
+
+//Resparameter
+// const dispayColors = (...colors: Array<string>) => {
+//   return colors.map((color) => {
+//     console.log(color);
+//   });
+// };
+
+// const dispayColors = (...colors: string[]) => {
+//   return colors.map((color) => {
+//     console.log(color);
+//   });
+// };
+
+// dispayColors("red", "green");
+
+const tinhTongParams = (...numbers: Array<number>) => {
+  let tong: number = 0;
+  numbers.forEach((num) => {
+    tong += num;
+  });
+  return tong;
+};
+
+console.log(tinhTongParams(1, 2, 3, 4, 5));
